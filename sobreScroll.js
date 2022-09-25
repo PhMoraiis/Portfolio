7708841a320629a69374f04c5f8394fb0329e0b1
Philipe Barbosa de Morais <101151994+PhMoraiis@users.noreply.github.com>
2022-09-25 19:08:18 -0300

    ARQUIVOS SOBRE

---
+++ sobreScroll.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.navbar',{reset: true});
+
+sr.reveal('.sobre', {duration: 1000});
+
+sr.reveal('.foto-sobre', {duration: 1000});
+
+sr.reveal('.foto-facts', {duration: 1000});
