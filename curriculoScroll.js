003f4cf3873b735f1f0271798b63d131f9bdff42
Philipe Barbosa de Morais <101151994+PhMoraiis@users.noreply.github.com>
2022-09-25 19:10:04 -0300

    ARQUIVOS CURRICULO

---
+++ curriculoScroll.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.navbar',{reset: true});
+
+sr.reveal('.exp', {duration: 1000});
+
+sr.reveal('.col-12', {duration: 1000});
+
+sr.reveal('.curriculo', {duration: 1000});
